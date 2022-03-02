a5178efe2365930038c83b19a4e45250d3c16cd4
juannobl <jpnorena@sura.com.co>
2022-03-02 16:07:35 -0500

    se agregan validaciones y RecyclerView

--- app/src/main/java/com/example/testapp/FirstFragment.kt
+++ app/src/main/java/com/example/testapp/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.testapp.databinding.FragmentFirstBinding
 
 /**
@@ -13,18 +14,21 @@ import com.example.testapp.databinding.FragmentFirstBinding
  */
 class FirstFragment : Fragment() {
 
-private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentFirstBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    val mensajes =  arrayOf("UCO", "Profesor", "SURA", "Medico", "Amigo", "Propanda", "Bancolombia", "Davivienda","Medico", "Amigo", "Propanda", "Bancolombia", "Davivienda","Medico", "Amigo", "Propanda", "Bancolombia", "Davivienda")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-      _binding = FragmentFirstBinding.inflate(inflater, container, false)
-      return binding.root
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        recyclerViewBasic()
+        return binding.root
 
     }
 
@@ -33,11 +37,21 @@ private var _binding: FragmentFirstBinding? = null
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            println("Ingresando al segundo fragmento")
+
         }
     }
 
-override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun recyclerViewBasic() {
+        val viewManager = LinearLayoutManager(this.context)
+        val viewAdapter = MensajesAdapterBasic(mensajes)
+
+        binding.recyclerView.layoutManager = viewManager
+        binding.recyclerView.adapter = viewAdapter
     }
 }
--- app/src/main/java/com/example/testapp/SecondFragment.kt
+++ app/src/main/java/com/example/testapp/SecondFragment.kt
@@ -1,19 +1,23 @@
 package com.example.testapp
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.testapp.databinding.FragmentSecondBinding
+import com.google.android.material.snackbar.Snackbar
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
-private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentSecondBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -23,19 +27,45 @@ private var _binding: FragmentSecondBinding? = null
         savedInstanceState: Bundle?
     ): View? {
 
-      _binding = FragmentSecondBinding.inflate(inflater, container, false)
-      return binding.root
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
 
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            // findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            val email = binding.editTextTo.text as CharSequence
+            if (email.isValidEmail()) {
+                if (binding.editTextSubject.text.isEmpty()) {
+                    binding.editTextSubject.requestFocus()
+                    binding.editTextSubject.setError("El Asunto no puede estar vacío")
+
+                } else {
+                    if (binding.editTextMessage.text.isEmpty()) {
+                        binding.editTextMessage.requestFocus()
+                        binding.editTextMessage.setError("El Mensaje no puede estar vacío")
+                    } else {
+                        Snackbar.make(view, "Enviando correo a $email ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show()
+                    }
+                }
+
+            } else {
+                binding.editTextTo.requestFocus()
+                binding.editTextTo.setError("Correo Incorrecto")
+            }
         }
+
     }
-override fun onDestroyView() {
+
+    private fun CharSequence?.isValidEmail() =
+        !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
